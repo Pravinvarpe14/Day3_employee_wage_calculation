1f627ca5cae91db24f0b3492db44a3801035a97e
pravin varpe <pravinvarpe14@gmail.com>
2022-06-21 23:13:18 +0530

    Verifying Employee is Present or Absent

--- src/employeewage/Employeewage.java
+++ src/employeewage/Employeewage.java
@@ -4,7 +4,14 @@ public class Employeewage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage");
-
+		
+		// Employee Present or Absent
+		int employeePresent = 1;
+		int isPresent = (int) (Math.floor(Math.random() * 10) % 2);
+		if (isPresent == employeePresent)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
